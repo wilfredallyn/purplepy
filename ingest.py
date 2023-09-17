22264d0afe6dea2e9bc14d2824afc1213b0e841b
Wilfred Allyn <wilfredallyn@protonmail.com>
2023-09-17 08:43:28 -0400

    setup sqlalchemy

--- ingest.py
+++ ingest.py
@@ -2,31 +2,24 @@ from query import query_events
 from sqlalchemy import create_engine
 
 
-def temp():
-    pass
-
-
 def init_db(client):
     engine = create_engine("postgresql://postgres@localhost:5432/postgres")
 
     npub = "npub1dergggklka99wwrs92yz8wdjs952h2ux2ha2ed598ngwu9w7a6fsh9xzpc"
     df = query_events(client, npub)
-    df["tags"] = df["tags"].apply(lambda x: {item[0]: item[1] for item in x})
-    df[df.columns[:-1]].to_sql(
-        "events", engine, if_exists="append", index=True, method="multi"
+    df["tags_relay_url"] = df["tags"].apply(
+        lambda x: {item[0]: item[2] for item in x if len(item) > 2}
     )
+    df["tags"] = df["tags"].apply(lambda x: {item[0]: item[1] for item in x})
 
-    # from sqlalchemy.orm import sessionmaker
-    # Session = sessionmaker(bind=engine)
-
-    # session = Session()
-
-    # for index, row in df.iterrows():
-    #     session.add(Model(**row.to_dict()))
-    #     if index % 1000 == 0:  # adjust the batch size as per your requirements
-    #         session.commit()
-    # session.commit()
-
-
-# def run_ingestion():
-#     npub =
+    engine = create_engine("postgresql://postgres@localhost:5432/postgres")
+    df.to_sql(
+        "events",
+        engine,
+        if_exists="replace",  # append
+        method="multi",
+        dtype={
+            "tags": dialects.postgresql.JSONB,
+            "tags_relay_url": dialects.postgresql.JSONB,
+        },
+    )
--- query.py
+++ query.py
@@ -4,6 +4,25 @@ from nostr_sdk import Keys, Client, EventBuilder, Filter, PublicKey
 from datetime import timedelta
 import time
 
+from sqlalchemy import create_engine, Column, BigInteger, String
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class Events(Base):
+    __tablename__ = "events"
+    id = Column(String, primary_key=True)
+    created_at = Column(BigInteger)
+    kind = Column(BigInteger)
+    tags = Column(JSONB)
+    tags_relay_url = Column(JSONB)
+    pubkey = Column(String)
+    sig = Column(String)
+    content = Column(String)
+
 
 def query_events(client, kind=None, num_limit=1000):
     if kind:
@@ -23,6 +42,14 @@ def query_events_by_author(client, npub):
     return df
 
 
-def query_db(engine, sql_query):
+def query_events_db(engine):
     # query nested jsonb: sql_query = "SELECT * FROM events WHERE tags->>'t' = 'sm41623576'";
-    return pd.read_sql(sql_query, engine)
+    # return pd.read_sql(sql_query, engine)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        results = session.query(Events).all()
+
+    # Convert the results into a pandas DataFrame
+    df = pd.DataFrame([r.__dict__ for r in results])
+    df = df.drop("_sa_instance_state", axis=1)
+    return df
