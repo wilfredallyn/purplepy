4bd2be3508f2a57661fd4a8eb7a953464681f484
Wilfred Allyn <wilfredallyn@protonmail.com>
2023-09-24 14:03:28 -0400

    filter by days

--- query.py
+++ query.py
@@ -1,8 +1,9 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 import pandas as pd
 import json
-from nostr_sdk import Keys, Client, Filter, Options, PublicKey
+from nostr_sdk import Keys, Client, Filter, Options, PublicKey, Timestamp
 from sqla_models import Events
+from utils import postprocess
 
 
 def init_client():
@@ -26,17 +27,17 @@ def query_events(client, kind=None, num_limit=1000):
         filter = Filter().limit(num_limit)
     events = client.get_events_of([filter], timedelta(seconds=50))
     df = pd.DataFrame([json.loads(event.as_json()) for event in events]).set_index("id")
-    return df
+    return postprocess(df)
 
 
-def query_events_by_author(client, npub):
+def query_events_by_author(client, npub, timeout_secs=10):
     # fix: check if hex or bech32
     # PublicKey.from_hex('22dd8df1fed1da2574c4917146d93dcb679549aeead8f98cbbaf166d183662ad').to_bech32()
     pk = PublicKey.from_bech32(npub)
     filter = Filter().author(pk.to_hex()).limit(10)
-    events = client.get_events_of([filter], timedelta(seconds=10))
+    events = client.get_events_of([filter], timedelta(seconds=timeout_secs))
     df = pd.DataFrame([json.loads(event.as_json()) for event in events]).set_index("id")
-    return df
+    return postprocess(df)
 
 
 def query_db(Session, npub):
@@ -46,6 +47,36 @@ def query_db(Session, npub):
     with Session() as session:
         results = session.query(Events).filter(Events.pubkey == pk).all()
     df = pd.DataFrame([r.__dict__ for r in results])
-    if "_sa_instance_state" in df.columns:
-        df = df.drop("_sa_instance_state", axis=1)
-    return df
+    return postprocess(df)
+
+
+def get_filter_timestamp(num_days: int):
+    ts = int((datetime.now() - timedelta(days=num_days)).timestamp())
+    return Timestamp.from_secs(ts)
+
+
+# def filter_days(filter: Filter, num_days: int):
+#     ts = int((datetime.now() - timedelta(days=num_days)).timestamp())
+#     return filter.since())
+
+
+# print("Getting events from relays...")
+# filter = Filter().authors([keys.public_key().to_hex()])
+# events = client.get_events_of([filter], timedelta(seconds=10))
+# for event in events:
+#     print(event.as_json())
+
+# filter = Filter().pubkey(pk).kind(4).since(Timestamp.now())
+# client.subscribe([filter])
+
+# write event
+# event = EventBuilder.new_text_note("Hello from Rust Nostr Python bindings!", []).to_event(keys)
+# event_id = client.send_event(event)
+
+# relay stats
+# while True:
+#     for url, relay in client.relays().items():
+#         stats = relay.stats()
+#         print(f"Relay: {url}")
+#         print(f"Connected: {relay.is_connected()}")
+#         print(f"Status: {relay.status()}")
