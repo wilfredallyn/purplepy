65211654b79eca3452b1838e0e586f60ece338c1
Wilfred Allyn <wilfredallyn@protonmail.com>
2023-11-25 10:00:23 -0500

    create weaviate records

--- purple_py/strfry.py
+++ purple_py/strfry.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import json
 import lmdb
 import os
@@ -47,19 +48,15 @@ def load_weaviate_data(event_json, batch, process_input):
 
 
 def query_db_for_record(client, process_fn, process_input):
-    print(STRFRY_PATH)
     env = lmdb.open(path=STRFRY_PATH, max_dbs=10)
     db_id = env.open_db(b"rasgueadb_defaultDb__Event__id")
     db_payload = env.open_db(b"rasgueadb_defaultDb__EventPayload")
 
     event_id_list = process_input["event_id_list"]
-    embedding_list = process_input["embedding_list"]
-
     client.batch.configure(batch_size=WEAVIATE_BATCH_SIZE)
     with client.batch as batch:
         with env.begin() as txn:
             db_id_cursor = txn.cursor(db=db_id)
-            # event_id = bytes.fromhex('fff8923d6db9a6d116edf5a0f4b747ede05b282cc939ca41d1461e221d94e7b5')
             for event_id in event_id_list:
                 event_id_bytes = bytes.fromhex(event_id)
                 if db_id_cursor.set_range(event_id_bytes):
@@ -67,31 +64,29 @@ def query_db_for_record(client, process_fn, process_input):
                     if k[:32] == event_id_bytes:
                         pl = txn.get(v, db=db_payload)
                         if pl:
-                            # event_json = pl[1:].decode("utf-8")
-                            # print(event_json)
                             event_json = json.loads(pl[1:].decode("utf-8"))
-                            pubkey = event_json["pubkey"]
-                            output = process_fn(event_json, batch, process_input)
-                            print(event_json)
+                            process_input = process_fn(event_json, batch, process_input)
                         else:
                             raise Exception("db corrupt!?")
                     else:
                         print("not found")
                 else:
                     print("not found")
+    return process_input
 
 
 def create_weaviate_record(event_json, batch, process_input):
-    pubkey_dict = process_input["pubkey_dict"]
     event_id_list = process_input["event_id_list"]
+    embedding_list = process_input["embedding_list"]
 
     pubkey = event_json["pubkey"]
     event_id = event_json["id"]
-    content = event_json["content"]
+    event_idx = event_id_list.index(event_id)
 
+    dt_utc = datetime.fromtimestamp(event_json["created_at"], timezone.utc)
     event_properties = {
-        "event_id": event_json["id"],
-        "created_at": event_json["created_at"],
+        "event_id": event_id,
+        "created_at": dt_utc.isoformat(),
         "pubkey": pubkey,
         "kind": event_json["kind"],
         "content": event_json["content"],
@@ -99,10 +94,10 @@ def create_weaviate_record(event_json, batch, process_input):
     batch.add_data_object(
         data_object=event_properties,
         class_name="Event",
-        # vector=emb,
+        vector=embedding_list[event_idx],
     )
-    if pubkey not in pubkey_dict:
-        pubkey_dict[pubkey] = None
+    if pubkey not in process_input["pubkey_dict"]:
+        process_input["pubkey_dict"][pubkey] = None
         user_properties = {
             "pubkey": pubkey,
         }
@@ -110,19 +105,4 @@ def create_weaviate_record(event_json, batch, process_input):
             data_object=user_properties,
             class_name="User",
         )
-
-
-"""
-event_id_list, content_list = read_strfy_db(
-    client,
-    event_process_fn=get_content_for_embeddings,
-    event_process_input=content_list,
-)
-
-query_db_for_record?
-read_strfy_db(
-    client,
-    event_process_fn=load_weaviate_data,
-    event_process_input=(event_id_list, content_list),
-)
-"""
+    return process_input
