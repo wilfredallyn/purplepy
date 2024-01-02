6ec9297608f0dd3570ebb6972600d77264c72101
Wilfred Allyn <wilfredallyn@protonmail.com>
2024-01-02 09:19:57 -0500

    create home summary data table

--- purple_py/pages/home.py
+++ purple_py/pages/home.py
@@ -1,7 +1,9 @@
 import dash
-from dash import html
+from dash import html, dash_table
+import dash_bootstrap_components as dbc
 from purple_py.db import client
 from purple_py.query import get_kind_counts
+from purple_py.utils import kind_name_dict
 
 
 dash.register_page(__name__, path="/", name="Home")  # '/' is home page
@@ -10,16 +12,48 @@ dash.register_page(__name__, path="/", name="Home")  # '/' is home page
 def layout():
     layout = html.Div(
         [
-            html.P(f"{get_db_summary(client)}"),
+            dbc.Container(
+                [
+                    dbc.Row(
+                        html.P(
+                            "This database contains mostly events between 2023-11-14 and 2023-11-21. It contains the following events:"
+                        )
+                    ),
+                    dbc.Row(
+                        dbc.Col(
+                            [get_db_summary(client)],
+                            width={
+                                "size": 10,
+                                "offset": 1,
+                            },
+                        )
+                    ),
+                ],
+                fluid=True,
+            )
         ]
     )
     return layout
 
 
 def get_db_summary(client):
-    df = get_kind_counts(client)
-    kind_string = ", ".join(
-        [f"{row['kind']} ({row['count']} events)" for _, row in df.iterrows()]
+    df = (
+        get_kind_counts(client)
+        .sort_values("count", ascending=False)
+        .rename(columns={"count": "num_events"})
+        .rename(columns={"kind": "kind_num"})
     )
-    output = f"This database contains mostly events between 2023-11-14 and 2023-11-21. It contains the following kinds: {kind_string}"
-    return output
+    df["kind_num"] = df["kind_num"].astype(int)
+    df["kind_name"] = df["kind_num"].map(kind_name_dict)
+
+    cols = ["kind_num", "kind_name", "num_events"]
+
+    output_table = dash_table.DataTable(
+        id="table",
+        columns=[{"name": col, "id": col} for col in cols],
+        data=df[cols].to_dict("records"),
+        fill_width=False,
+        style_cell={"textAlign": "center"},
+        sort_action="native",
+    )
+    return output_table
